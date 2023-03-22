e1fccb6aaaabbaf6d3a5b5ad6f90416e54225151
tiffany <ting11222001@gmail.com>
2023-03-22 12:02:26 +1030

    feat: 🎸 add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,11 @@ import Header from '@/components/Header';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-      <Content />
-    </div>
+      <div className="sm:p-8 px-4 py-8 w-full h-screen bg-[#f9fafe]">
+        <Content />
+      </div>
+    </>
   )
 }
