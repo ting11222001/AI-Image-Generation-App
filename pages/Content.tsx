812ad6d78a19497cee3b5190ab79e994a4085bfc
tiffany <ting11222001@gmail.com>
2023-03-22 12:10:27 +1030

    feat: 🎸 search text layout

--- pages/Content.tsx
+++ pages/Content.tsx
@@ -6,8 +6,9 @@ import Card from "@/components/Card";
 type Props = {}
 
 const Content = (props: Props) => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState('');
   return (
     <div className='max-w-7xl mx-auto'>
       <div>
@@ -24,6 +25,9 @@ const Content = (props: Props) => {
           </div>
         ) : (
           <>
+            {searchText && (
+              <h2 className='font-medium text-[#666e75] text-xl mb-3'>Showing results for <span className='text-[#222328]'>{searchText}</span></h2>
+            )}
           </>
         )}
       </div>
