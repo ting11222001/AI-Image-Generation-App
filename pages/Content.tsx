f6aebae11a2e23116bf928500acc8090b9004d8a
tiffany <ting11222001@gmail.com>
2023-03-22 12:34:16 +1030

    feat: 🎸 create search results layout

--- pages/Content.tsx
+++ pages/Content.tsx
@@ -3,7 +3,30 @@ import FormField from "@/components/FormField";
 import Loader from "@/components/Loader";
 import Card from "@/components/Card";
 
-type Props = {}
+type Props = {
+  data?: Array<any>;
+  title?: string;
+}
+
+const RenderCards = ({ data, title }: Props) => {
+  if (data && data.length > 0) {
+    return (
+      <>
+        {data.map((post) => {
+          <Card
+            key={post._id}
+            {...post}
+          />
+        })}
+      </>
+    )
+  }
+  return (
+    <>
+      <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+    </>
+  )
+}
 
 const Content = (props: Props) => {
   const [loading, setLoading] = useState(false);
@@ -28,6 +51,20 @@ const Content = (props: Props) => {
             {searchText && (
               <h2 className='font-medium text-[#666e75] text-xl mb-3'>Showing results for <span className='text-[#222328]'>{searchText}</span></h2>
             )}
+
+            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+              {searchText ? (
+                <RenderCards
+                  data={[]}
+                  title="No search results found"
+                />
+              ) : (
+                <RenderCards
+                  data={[]}
+                  title="No posts found"
+                />
+              )}
+            </div>
           </>
         )}
       </div>
