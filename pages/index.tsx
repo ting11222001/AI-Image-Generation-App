97988067619c746b7f4df8c3a120476b4140b24d
tiffany <ting11222001@gmail.com>
2023-03-22 13:20:36 +1030

    fix: 🐛 content layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="sm:p-8 px-4 py-8 w-full h-screen bg-[#f9fafe]">
+      <div className="bg-[#f9fafe]">
         <Content />
       </div>
     </>
