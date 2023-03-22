d0dcd251431551d18c08a67905d403381152e6de
tiffany <ting11222001@gmail.com>
2023-03-22 11:19:23 +1030

    feat: 🎸 create files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import React from "react";
+import logo from "../public/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link href="/">
+          <Image
+            className="w-28 object-contain"
+            src={logo}
+            alt="logo"
+            width={50}
+            height={50}
+            placeholder="blur"
+            blurDataURL="../public/vercel.svg"
+          />
+        </Link>
+      </div>
+    </>
   )
 }
