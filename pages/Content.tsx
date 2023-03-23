8bb893c0347af2995a62c0b9b29999733c734b14
tiffany <ting11222001@gmail.com>
2023-03-23 22:36:31 +1030

    fix: 🐛 add fragment to data.map's multiple element

--- pages/Content.tsx
+++ pages/Content.tsx
@@ -10,14 +10,14 @@ type Props = {
 
 const RenderCards = ({ data, title }: Props) => {
   if (data && data.length > 0) {
-    return (
+    return <>{
       data.map((post) =>
         <Card
           key={post._id}
           {...post}
         />
       )
-    )
+    }</>
   }
   return (
     <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
